b4c8d8243313322bb05c99be11b18bbb0de1889b
JOswaldoVM <99057440+JOswaldoVM@users.noreply.github.com>
2022-03-31 22:20:35 -0600

    Add files via upload

---
+++ AgendaPrincipal.java
@@ -0,0 +1,10 @@
+
+public class AgendaPrincipal {
+
+    
+    public static void main(String[] args) {
+        //JFrameMenu menu = new JFrameMenu();
+        JFrameIngreso ingreso = new JFrameIngreso();
+    }
+    
+}
---
+++ JFrameIngreso.java
@@ -0,0 +1,22 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class JFrameIngreso extends JFrame{
+    JPanelIngreso ingreso = new JPanelIngreso();
+    public JFrameIngreso(){
+        componentes();
+    }
+    public void componentes(){     
+        this.setTitle("Iniciar sesión");
+        this.setSize(500, 260);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setContentPane(ingreso);
+        
+    }
+
+}
